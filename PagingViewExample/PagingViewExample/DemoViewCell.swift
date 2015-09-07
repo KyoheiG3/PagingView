7a5690cde2d6b1df549f11f961255562ad7cdf55
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2015-09-07 11:48:26 +0900

    add demo app

---
+++ PagingViewExample/PagingViewExample/DemoViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DemoViewCell.swift
+//  PagingViewExample
+//
+//  Created by Kyohei Ito on 2015/09/07.
+//  Copyright © 2015年 kyohei_ito. All rights reserved.
+//
+
+import UIKit
+import PagingView
+
+class DemoViewCell: PagingViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+
+}
--- PagingViewExample/PagingViewExample/DemoViewController.swift
+++ PagingViewExample/PagingViewExample/DemoViewController.swift
@@ -7,29 +7,49 @@
 //
 
 import UIKit
+import PagingView
 
 class DemoViewController: UIViewController {
+    @IBOutlet weak var pagingView: PagingView!
+    
+    let imageNames: [String] = ["1", "2", "3", "4", "5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        pagingView.dataSource = self
+        pagingView.delegate = self
+        
+        let nib = UINib(nibName: "DemoViewCell", bundle: nil)
+        pagingView.registerNib(nib, forCellWithReuseIdentifier: "DemoViewCell")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension DemoViewController: PagingViewDataSource, PagingViewDelegate, UIScrollViewDelegate {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if let centerCell = pagingView.visibleCenterCell() {
+            let imageName = imageNames[centerCell.indexPath.item]
+            title = imageName
+        }
     }
-    */
-
+    
+    func pagingView(pagingView: PagingView, numberOfItemsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func pagingView(pagingView: PagingView, cellForItemAtIndexPath indexPath: NSIndexPath) -> PagingViewCell {
+        let cell = pagingView.dequeueReusableCellWithReuseIdentifier("DemoViewCell")
+        if let cell = cell as? DemoViewCell {
+            let imageName = imageNames[indexPath.item]
+            cell.imageView.image = UIImage(named: imageName)
+        }
+        
+        return cell
+    }
 }
